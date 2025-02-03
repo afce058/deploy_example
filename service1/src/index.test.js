433467e50c1a19300226226166cdd0a61ead5e37
Andres Felipe Cardona Escobar <acardona@MacBook-Pro-de-Andres.local>
2025-02-03 15:16:13 -0500

    resources modified

--- service1/src/index.test.js
+++ service1/src/index.test.js
@@ -2,18 +2,18 @@
 const request = require('supertest');
 const app = require('./index');
 
-describe('GET /api/endpoint1', () => {
+describe('GET /service1/api/endpoint1', () => {
   it('should return status 200 and a success message', async () => {
-    const response = await request(app).get('/api/endpoint1');
+    const response = await request(app).get('/service1/api/endpoint1');
 
     expect(response.status).toBe(200);
     expect(response.body.message).toBe('Endpoint 1 service 1');
   });
 });
 
-describe('GET /api/endpoint2', () => {
+describe('GET /service1/api/endpoint2', () => {
     it('should return status 200 and a success message', async () => {
-      const response = await request(app).get('/api/endpoint2');
+      const response = await request(app).get('/service1/api/endpoint2');
   
       expect(response.status).toBe(200);
       expect(response.body.message).toBe('Endpoint 2 service 1');
